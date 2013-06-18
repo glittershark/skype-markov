2cfd423fccebb04b861699a0959ad808a54c2b7d
Griffin Smith <smith@visionlink.org>
2013-06-17 23:40:51 -0600

    Initial Commit
    Markov chain generator from https://gist.github.com/agiliq/131679
    Along with sqlite script to load some data from Skype. Currently
    hardcoded with my Skype username.

---
+++ init.sql
@@ -0,0 +1,55 @@
+ATTACH DATABASE '/home/smith/.Skype/wildgriffin45/main.db' AS skype;
+ATTACH DATABASE 'markov.db' AS markov;
+
+-- Create the tables that cache data from Skype {{{
+
+CREATE TABLE IF NOT EXISTS markov.people (
+  id        INTEGER PRIMARY KEY AUTOINCREMENT,
+  skypename TEXT,
+  fullname  TEXT
+);
+
+CREATE TABLE IF NOT EXISTS markov.chatlog_raw (
+  sender_id    INTEGER,
+  message_body TEXT,
+  -- TODO: chat_id TEXT
+
+  FOREIGN KEY ( sender_id ) REFERENCES markov.people ( id )
+);
+
+-- }}}
+
+-- Insert the Skype chatlog data into those tables {{{
+
+INSERT INTO markov.people ( skypename, fullname )
+(
+  SELECT a.skypename, a.realname FROM skype.Accounts AS a
+  -- make sure we don't get anyone who's never said anything
+  INNER JOIN skype.Messages AS m
+    ON a.skypename = m.author
+);
+
+INSERT INTO markov.chatlog_raw ( sender_id, message_body )
+(
+  SELECT p.id, m.body_xml FROM skype.Messages AS m
+  JOIN markov.people AS p
+    ON m.author = p.skypename
+);
+
+-- }}}
+
+-- Create data structure schemas {{{
+
+CREATE TABLE IF NOT EXISTS markov.chains (
+  sender_id INTEGER,
+  word1     TEXT,
+  word2     TEXT,
+  word3     TEXT,
+
+  FOREIGN KEY ( sender_id ) REFERENCES markov.people ( id )
+);
+
+-- }}}
+
+-- vim: set foldmethod=marker expandtab tabstop=2 shiftwidth=2 syntax=sql:
+
