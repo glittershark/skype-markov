a472075667de53e20ec4066fcd262f73bbe50ef0
Griffin Smith <smith@visionlink.org>
2013-06-20 18:37:49 -0600

    Initial implementation of Markov chain generator
    This is the initial working implementation of the Skype-seeded Markov
    natural language chain generator. The username-specific stuff is
    commented out atm until I can figure out what's going on with Skype's
    data structures, and there's no user interface (everything is done
    essentially by modifying the if __name__ == '__main__' block).

--- init.sql
+++ init.sql
@@ -1,54 +1,61 @@
--- replace <your_username> and <your_skype_name with
--- the path to your home folder and your skype name, respectively
-ATTACH DATABASE '/home/<your_username>/.Skype/<your_skype_name>/main.db' AS skype;
-ATTACH DATABASE 'markov.db' AS markov;
+ATTACH DATABASE '%(home_folder)s/.Skype/%(skype_username)s/main.db' AS skype;
 
 -- Create the tables that cache data from Skype {{{
 
-CREATE TABLE IF NOT EXISTS markov.people (
+CREATE TABLE IF NOT EXISTS people (
   id        INTEGER PRIMARY KEY AUTOINCREMENT,
   skypename TEXT,
   fullname  TEXT
 );
 
-CREATE TABLE IF NOT EXISTS markov.chatlog_raw (
+CREATE TABLE IF NOT EXISTS chatlog_raw (
   sender_id    INTEGER,
-  message_body TEXT,
+  message_body TEXT UNIQUE ON CONFLICT IGNORE,
   -- TODO: chat_id TEXT
 
-  FOREIGN KEY ( sender_id ) REFERENCES markov.people ( id )
+  FOREIGN KEY ( sender_id ) REFERENCES people ( id )
 );
 
 -- }}}
 
 -- Insert the Skype chatlog data into those tables {{{
 
-INSERT INTO markov.people ( skypename, fullname )
-(
-  SELECT a.skypename, a.realname FROM skype.Accounts AS a
+INSERT INTO people ( skypename, fullname )
+  SELECT a.skypename, a.fullname FROM skype.Accounts AS a
   -- make sure we don't get anyone who's never said anything
   INNER JOIN skype.Messages AS m
     ON a.skypename = m.author
-);
+;
 
-INSERT INTO markov.chatlog_raw ( sender_id, message_body )
-(
+INSERT INTO chatlog_raw ( sender_id, message_body )
   SELECT p.id, m.body_xml FROM skype.Messages AS m
-  JOIN markov.people AS p
+  JOIN people AS p
     ON m.author = p.skypename
-);
+;
 
 -- }}}
 
 -- Create data structure schemas {{{
 
-CREATE TABLE IF NOT EXISTS markov.chains (
-  sender_id INTEGER,
-  word1     TEXT,
-  word2     TEXT,
-  word3     TEXT,
+-- NOTE: split source two words (w1, w2) and third word into seperate column so we can
+--       have easier joins
 
-  FOREIGN KEY ( sender_id ) REFERENCES markov.people ( id )
+CREATE TABLE IF NOT EXISTS chain_stems (
+  id         INTEGER PRIMARY KEY AUTOINCREMENT,
+  sender_id  INTEGER,
+  word1      TEXT,
+  word2      TEXT,
+
+  FOREIGN KEY ( sender_id ) REFERENCES people ( id ),
+  UNIQUE ( word1, word2 )
+);
+
+CREATE TABLE IF NOT EXISTS chain_words (
+  stem_id     INTEGER,
+  word        TEXT,
+  probability INTEGER NOT NULL DEFAULT 1,
+
+  FOREIGN KEY ( stem_id ) REFERENCES chain_stems ( id )
 );
 
 -- }}}
--- markov.py
+++ markov.py
@@ -1,60 +1,111 @@
-import random
+import sqlite3
 
-class Markov(object):
+def triples(words):
+  """ Generates triples from the given data string. So if our string were
+      "What a lovely day", we'd generate (What, a, lovely) and then
+      (a, lovely, day).
+  """
 
-	def __init__(self, open_file):
-		self.cache = {}
-		self.open_file = open_file
-		self.words = self.file_to_words()
-		self.word_size = len(self.words)
-		self.database()
+  if len(words) < 3:
+    return
 
+  for i in range(len(words) - 2):
+    yield (words[i], words[i+1], words[i+2])
 
-	def file_to_words(self):
-		self.open_file.seek(0)
-		data = self.open_file.read()
-		words = data.split()
-		return words
+def get_random_seed_pair(conn):
+  c = conn.cursor()
+  c.execute("""SELECT word1, word2
+               FROM chain_stems
+               JOIN people
+                 ON chain_stems.sender_id = people.id
+--               WHERE skypename = ?
+               ORDER BY RANDOM()
+               LIMIT 1""") #, (username,))
+  return c.fetchone()
 
+def generate(username, num_words, db_file="./markov.db"):
+  conn = sqlite3.connect(db_file)
+  w1, w2 = get_random_seed_pair
 
-	def triples(self):
-		""" Generates triples from the given data string. So if our string were
-				"What a lovely day", we'd generate (What, a, lovely) and then
-				(a, lovely, day).
-		"""
+  gen_words = []
+  for i in xrange(num_words):
+    gen_words.append(w1)
+    c2 = conn.cursor()
+    c2.execute("""SELECT word
+                 FROM chain_stems
+                 JOIN chain_words
+                   ON chain_stems.id   = chain_words.stem_id
+--                 JOIN people
+--                   ON chain_stems.sender_id = people.id
+                 WHERE lower(word1) = lower(?)
+                   AND lower(word2) = lower(?)
+--                   AND skypename  = ?
+                 ORDER BY RANDOM() * probability DESC
+                 LIMIT 1;""",
+                 (w1, w2)) #, username))
+    row = c2.fetchone()
+    if row is not None:
+      w1 = w2
+      w2 = row[0]
+    else:
+      return ' '.join(gen_words)
+  gen_words.append(w2)
+  return ' '.join(gen_words)
 
-		if len(self.words) < 3:
-			return
+def load_from_skype(home_folder, skype_username, db_file="./markov.db", do_initial_load=True):
+  conn = sqlite3.connect(db_file)
+  conn.row_factory = sqlite3.Row
 
-		for i in range(len(self.words) - 2):
-			yield (self.words[i], self.words[i+1], self.words[i+2])
+  try:
+    ## Initialize the database and load the initial data from Skype
+    if do_initial_load:
+      with open("./init.sql", 'r') as f:
+        conn.executescript(f.read() % {"home_folder": home_folder, "skype_username": skype_username})
 
-	def database(self):
-		for w1, w2, w3 in self.triples():
-			key = (w1, w2)
-			if key in self.cache:
-				self.cache[key].append(w3)
-			else:
-				self.cache[key] = [w3]
+    ## Go through all the messages...
+    for row in conn.execute('SELECT sender_id, message_body FROM main.chatlog_raw'):
+      if row['message_body'] is None:
+        continue
+      ## Split the message into words
+      words = row['message_body'].split()
+      for triple in triples(words):
+        ## Test if we already have these stem words
+        #c = conn.cursor()
+        #if len(c.execute("""
+            #SELECT 1 FROM main.chain_stems as cs
+                #WHERE cs.word1     = ?
+                #AND   cs.word2     = ?
+                #AND   cs.sender_id = ?""",
+                #(triple[0], triple[1], row["sender_id"])).fetchall()) == 0:
+        try:
+          c2 = conn.cursor()
+          c2.execute("""INSERT INTO chain_stems (word1, word2, sender_id) VALUES (?, ?, ?)""",
+              (triple[0], triple[1], row["sender_id"]))
+          conn.execute("""INSERT INTO chain_words (stem_id, word) VALUES (?, ?)""",
+              (c2.lastrowid, triple[2]))
+        except sqlite3.IntegrityError:
+          conn.execute("""UPDATE main.chain_words SET probability = probability + 1
+              WHERE word = ?""", (triple[2],))
+  finally:
+    conn.commit()
 
-	def generate_markov_text(self, size=25):
-		seed = random.randint(0, self.word_size-3)
-		seed_word, next_word = self.words[seed], self.words[seed+1]
-		w1, w2 = seed_word, next_word
-		gen_words = []
-		for i in xrange(size):
-			gen_words.append(w1)
-			w1, w2 = w2, random.choice(self.cache[(w1, w2)])
-		gen_words.append(w2)
-		return ' '.join(gen_words)
+def get_random_username(db_file="./markov.db"):
+  pass
+  conn = sqlite3.connect(db_file)
+  c = conn.cursor()
+  c.execute("""SELECT skypename FROM people
+             ORDER BY RANDOM()
+             LIMIT 1""")
+  return c.fetchone()[0]
+
 
 if __name__ == "__main__":
-  import sys
-  if len(sys.argv) > 1:
-    with open(sys.argv[1], 'r') as f:
-      markov = Markov(f)
-  else:
-    markov = Markov(sys.stdin)
+  #import argparse
+  #parser = argparse.ArgumentParser()
+  #parser.add_argument("command", help="[load|generate]")
+  #args = parser.parse_args()
 
-  print markov.generate_markov_text()
+  #load_from_skype("/home/smith", "wildgriffin45", do_initial_load=True)
+  for i in xrange(50):
+    print generate(None, 400)
 
